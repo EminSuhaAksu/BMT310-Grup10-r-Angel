a0f0e90ff319e3974f78c51e07cc03b25aac39d0
Bora IŞIKOĞLU <79979746+boraisikoglu@users.noreply.github.com>
2021-05-26 01:55:14 +0300

    Update ArduinoServoControl.ino

--- r-Angel/ArduinoServoControl.ino
+++ r-Angel/ArduinoServoControl.ino
@@ -1,20 +1,24 @@
 #include <Servo.h>
 int pin=3;
+int pin2=5;
 Servo Servo1;
 int x=0;
 String str;
 void setup() {
+  pinMode(pin2, OUTPUT);
   Serial.begin(2000000);
   Servo1.attach(pin);
 }
 
 void loop() {
   
-  
+  digitalWrite(pin2,HIGH);
   while(!Serial.available()){
+    digitalWrite(pin2,LOW);
     Servo1.write(x);
-    delay(10);
+    
   }
+  digitalWrite(pin2,HIGH);
   Servo1.write(x);
   str=Serial.readString();
   Servo1.write(x);
@@ -22,6 +26,4 @@ void loop() {
   Servo1.write(x);
     
   
-  
-
 }
